4225375cbf37560a9c3ba83da08b2fd03665aebe
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-01-06 21:47:11 +0100

    Update pom file. Adding commandLineRunner

---
+++ src/main/java/spring/aopdemo/AopDemoApplication.java
@@ -0,0 +1,21 @@
+package spring.aopdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hello world from AOP DEMO!");
+		};
+	}
+}
