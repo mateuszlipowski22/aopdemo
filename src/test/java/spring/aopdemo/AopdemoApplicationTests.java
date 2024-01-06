a2c2d0e71d6ae70119a28ce1d619ee5575aba1d1
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-01-06 21:38:48 +0100

    Initial commit

---
+++ src/test/java/spring/aopdemo/AopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.aopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
